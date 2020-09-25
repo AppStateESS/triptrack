420a7659423124a7686fd38c97c21d3cda14da30
Matt McNaney <mcnaneym@appstate.edu>
2020-09-25 12:52:33 -0400

    Saving organization

--- class/Factory/OrganizationFactory.php
+++ class/Factory/OrganizationFactory.php
@@ -8,8 +8,9 @@
 namespace triptrack\Factory;
 
 use phpws2\Database;
+use Canopy\Request;
 
-class OrganizationFactory
+class OrganizationFactory extends \phpws2\ResourceFactory
 {
 
     public static function exists()
@@ -26,6 +27,13 @@ class OrganizationFactory
         $db = Database::getDB();
         $tbl = $db->addTable('trip_organization');
         return $db->select();
+    }
+
+    public static function post(Request $request)
+    {
+        $org = new \triptrack\Resource\Organization;
+        $org->name = $request->pullPostString('name');
+        self::saveResource($org);
     }
 
 }
