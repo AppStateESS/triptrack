0f58f3a23222cb8b182df3d2ed4477c7c23ac637
Matt McNaney <mcnaneym@appstate.edu>
2020-10-14 13:26:03 -0400

    Moved in list

--- javascript/TripList/index.jsx
+++ javascript/TripList/index.jsx
@@ -1,18 +1,73 @@
 'use strict'
-import React, {Component} from 'react'
+import React, {useState, useEffect} from 'react'
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
-import List from './List'
+import Grid from './Grid'
+import Menu from './Menu'
+import Message from '../Share/Message'
+import {getList} from '../api/Fetch'
 
-export default class TripList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
+const TripList = () => {
+  const [trips, setTrips] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState('danger')
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  const load = async () => {
+    let response = await getList('./triptrack/Admin/Trip/', {search})
+    if (response === false) {
+      setMessage('Error: could not load trip list')
+      setMessageType('danger')
+      setLoading(false)
+    } else {
+      console.log(response)
+      if (response.length > 0) {
+        setTrips(response)
+      }
+      setLoading(false)
+    }
+  }
+  const sendSearch = () => {
+    load()
   }
 
-  render() {
+  const deleteRow = () => {}
+
+  const update = () => {}
+
+  if (loading) {
+    return <div>Loading trips...</div>
+  } else if (trips.length === 0) {
     return (
       <div>
-        <List />
+        <Message message={message} type={messageType} />
+        <Menu
+          showModal={() => {
+            setShowModal(true)
+          }}
+        />
+        <p>No organizations found.</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Menu
+          search={search}
+          setSearch={setSearch}
+          sendSearch={sendSearch}
+          showModal={() => {
+            setShowModal(true)
+          }}
+        />
+        <Message message={message} type={messageType} />
+        <Grid trips={trips} edit={update} deleteRow={deleteRow} />
+        {modal}
       </div>
     )
   }
