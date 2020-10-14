1eb9864fcdb7917e861ac226ca46105cf8aad52d
Matt McNaney <mcnaneym@appstate.edu>
2020-10-14 13:12:37 -0400

    Removed organizationId, added phone variable.

--- class/Resource/Member.php
+++ class/Resource/Member.php
@@ -14,7 +14,7 @@ class Member extends \phpws2\Resource
     protected $email;
     protected $firstName;
     protected $lastName;
-    protected $organizationId;
+    protected $phone;
     protected $username;
     protected $table = 'trip_member';
 
@@ -23,9 +23,8 @@ class Member extends \phpws2\Resource
         $this->bannerId = new \phpws2\Variable\IntegerVar(0, 'bannerId');
         $this->email = new \phpws2\Variable\Email(null, 'email', 100);
         $this->firstName = new \ phpws2\Variable\TextOnly(null, 'firstName', 50);
+        $this->phone = new \ phpws2\Variable\TextOnly(null, 'phone', 15);
         $this->lastName = new \phpws2\Variable\TextOnly(null, 'lastName', 50);
-        $this->organizationId = new \phpws2\Variable\IntegerVar(0,
-                'organizationId');
         $this->username = new \phpws2\Variable\TextOnly(null, 'username', 50);
         parent::__construct();
     }
