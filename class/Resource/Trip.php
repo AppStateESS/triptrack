2bb40a6199ad71a126ec7d3c0e7c29689ead07b0
Matt McNaney <mcnaneym@appstate.edu>
2020-10-14 13:13:09 -0400

    Added approved, contactEmail, secondary email, submitted username

--- class/Resource/Trip.php
+++ class/Resource/Trip.php
@@ -11,19 +11,23 @@ class Trip extends \phpws2\Resource
 {
 
     protected $additionalMembers;
-    protected $host;
+    protected $approved;
     protected $contactName;
+    protected $contactEmail;
     protected $contactPhone;
     protected $destinationCity;
     protected $destinationCountry;
     protected $destinationState;
+    protected $host;
     protected $housingAddress;
     protected $organizationId;
     protected $secContactName;
+    protected $secContactEmail;
     protected $secContactPhone;
     protected $submitDate;
     protected $submitEmail;
     protected $submitName;
+    protected $submitUsername;
     protected $timeDeparting;
     protected $timeEventStarts;
     protected $timeReturn;
@@ -35,13 +39,15 @@ class Trip extends \phpws2\Resource
         $this->additionalMembers = new \phpws2\Variable\TextOnly(null,
                 'additionalMembers');
         $this->additionalMembers->allowNull(true);
+        $this->approved = new \phpws2\Variable\BooleanVar(false, 'approved');
         $this->host = new \phpws2\Variable\TextOnly(null, 'host', 255);
         $this->contactName = new \phpws2\Variable\TextOnly(null, 'contactName',
                 255);
+        $this->contactEmail = new \phpws2\Variable\Email(null, 'contactEmail');
         $this->contactPhone = new \phpws2\Variable\PhoneNumber(null,
                 'contactPhone');
         $this->destinationCity = new \phpws2\Variable\TextOnly(null,
-                'destinationCity');
+                'destinationCity', 80);
         $this->destinationCountry = new \phpws2\Variable\TextOnly(null,
                 'destinationCountry', 2);
         $this->destinationState = new \phpws2\Variable\TextOnly(null,
@@ -52,11 +58,15 @@ class Trip extends \phpws2\Resource
                 'organizationId');
         $this->secContactName = new \phpws2\Variable\TextOnly(null,
                 'contactName', 255);
+        $this->secContactEmail = new \phpws2\Variable\Email(null,
+                'secContactEmail');
         $this->secContactPhone = new \phpws2\Variable\PhoneNumber(null,
                 'contactPhone');
         $this->submitDate = new \phpws2\Variable\DateTime(0, 'submitDate');
         $this->submitEmail = new \phpws2\Variable\Email(null, 'submitEmail');
-        $this->submitName = new \phpws2\Variable\TextOnly(null, 'submitName');
+        $this->submitUsername = new \phpws2\Variable\TextOnly(null,
+                'submitUsername', 50);
+        $this->submitName = new \phpws2\Variable\TextOnly(null, 'submitName', 60);
         $this->timeDeparting = new \phpws2\Variable\DateTime(0, 'timeDeparting');
         $this->timeEventStarts = new \phpws2\Variable\DateTime(0,
                 'timeEventStarts');
@@ -64,9 +74,7 @@ class Trip extends \phpws2\Resource
         $this->visitPurpose = new \phpws2\Variable\TextOnly(null,
                 'visitPurpose', 255);
         parent::__construct();
-        if ($this->submitDate->isEmpty()) {
-            $this->submitDate->stamp();
-        }
+        $this->submitDate->stamp();
     }
 
 }
