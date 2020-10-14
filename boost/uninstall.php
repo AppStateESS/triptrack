ea65a40464bffbfc5fb9a75bc62355769b64e243
Matt McNaney <mcnaneym@appstate.edu>
2020-10-14 13:11:18 -0400

    membertoorg table uninstalled

--- boost/uninstall.php
+++ boost/uninstall.php
@@ -1,32 +1,14 @@
 <?php
 
-/*
- * Copyright (C) 2017 Matthew McNaney <mcnaneym@appstate.edu>.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
-
 function triptrack_uninstall(&$content)
 {
     $db = \phpws2\Database::getDB();
     $db->buildTable('trip_membertotrip')->drop(true);
+    $db->buildTable('trip_membertoorg')->drop(true);
     $db->buildTable('trip_organization')->drop(true);
     $db->buildTable('trip_member')->drop(true);
-    $db->buildTable('trip_trip')->drop(true);
     $db->buildTable('trip_document')->drop(true);
+    $db->buildTable('trip_trip')->drop(true);
 
     // From previous version
     return true;
