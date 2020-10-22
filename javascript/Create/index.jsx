49a883051a82995662cc10e331f91e995ced0d02
Matt McNaney <mcnaneym@appstate.edu>
2020-10-22 09:20:27 -0400

    defaultTrip variable moved into other file
    Added ready condition for submit button

--- javascript/Create/index.jsx
+++ javascript/Create/index.jsx
@@ -1,40 +1,19 @@
 'use strict'
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import Host from '../TripForm/Host'
 import Contact from '../TripForm/Contact'
 import Submitter from '../TripForm/Submitter'
+import {defaultTrip} from '../TripForm/TripDefaults'
 
 /* global allowInternational, contactBannerRequired */
 
-const defaultTrip = {
-  additionalMembers: '',
-  fake: 1,
-  contactName: '',
-  contactEmail: '',
-  contactPhone: '',
-  destinationCity: '',
-  destinationCountry: 'United States',
-  destinationState: '',
-  host: '',
-  housingAddress: '',
-  organizationId: 0,
-  secContactName: '',
-  secContactEmail: '',
-  secContactPhone: '',
-  submitDate: 0,
-  submitEmail: '',
-  submitName: '',
-  submitUsername: '',
-  timeDeparting: 0,
-  timeEventStarts: 0,
-  timeReturn: 0,
-  visitPurpose: '',
-}
-
 const Create = ({allowInternational, contactBannerRequired}) => {
   const [Trip, setTrip] = useState(Object.assign({}, defaultTrip))
+  const [submitterReady, setSubmitterReady] = useState(false)
+  const [contactReady, setContactReady] = useState(false)
+  const [hostReady, setHostReady] = useState(false)
 
   const setFormElement = (key, value) => {
     Trip[key] = value
@@ -45,29 +24,41 @@ const Create = ({allowInternational, contactBannerRequired}) => {
     console.log(Trip)
   }
 
+  const saveReady = submitterReady && contactReady && hostReady
+
   return (
     <div>
+      <h2>Create Trip</h2>
       <a id="submitter-info"></a>
-      <Submitter Trip={Trip} setFormElement={setFormElement} />
+      <Submitter
+        Trip={Trip}
+        setFormElement={setFormElement}
+        ready={setSubmitterReady}
+      />
       <a id="host-info"></a>
       <Host
         Trip={Trip}
         setFormElement={setFormElement}
         allowInternational={allowInternational}
+        ready={setHostReady}
       />
       <a id="contact-info"></a>
       <Contact
         Trip={Trip}
         setFormElement={setFormElement}
         contactBannerRequired={contactBannerRequired}
+        ready={setContactReady}
       />
-      <button
-        className="btn btn-success"
-        onClick={() => {
-          postTrip()
-        }}>
-        Save and continue
-      </button>
+      <div className="text-center">
+        <button
+          className="btn btn-success"
+          onClick={() => {
+            postTrip()
+          }}
+          disabled={!saveReady}>
+          {saveReady ? 'Save and continue' : 'Fill in all fields above'}
+        </button>
+      </div>
     </div>
   )
 }
