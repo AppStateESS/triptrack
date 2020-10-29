32195ea0ea2fa7212f2e176a87ff3bdbb6f8e4b3
Matt McNaney <mcnaneym@appstate.edu>
2020-10-29 08:58:35 -0400

    Moved date picker into separate file.

---
+++ javascript/TripForm/Schedule.js
@@ -0,0 +1,78 @@
+'use strict'
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+const Schedule = ({Trip, setFormElement}) => {
+  let departingObj
+  if (Trip.timeDeparting) {
+    departingObj = new Date(Trip.timeDeparting * 1000)
+  } else {
+    departingObj = new Date()
+  }
+
+  let eventStarts
+  if (Trip.timeEventStarts) {
+    eventStarts = new Date(Trip.timeEventStarts * 1000)
+  } else {
+    eventStarts = new Date()
+  }
+
+  let departTime
+  if (Trip.timeDeparting) {
+    departTime = new Date(Trip.timeDeparting * 1000)
+  } else {
+    departTime = new Date()
+  }
+
+  return (
+    <fieldset className="mb-4">
+      <legend className="border-bottom mb-3">Schedule</legend>
+      <div>
+        <div className="row form-group">
+          <div className="col-sm-4">
+            <label className="d-block">Departure</label>
+            <DatePicker
+              selected={departingObj}
+              onChange={(value) => {
+                setFormElement('timeDeparting', value.getTime() / 1000)
+              }}
+              showTimeSelect
+              className="form-control"
+              dateFormat="MMM d, yyyy hh:mm"
+            />
+          </div>
+          <div className="col-sm-4">
+            <label className="d-block">Event start</label>
+            <DatePicker
+              selected={eventStarts}
+              onChange={(value) => {
+                setFormElement('timeEventStarts', value.getTime() / 1000)
+              }}
+              showTimeSelect
+              className="form-control"
+              dateFormat="MMM d, yyyy hh:mm"
+            />
+          </div>
+          <div className="col-sm-4">
+            <label className="d-block">Returning</label>
+            <DatePicker
+              selected={eventStarts}
+              onChange={(value) => {
+                setFormElement('timeDeparting', value.getTime() / 1000)
+              }}
+              showTimeSelect
+              className="form-control"
+              dateFormat="MMM d, yyyy hh:mm"
+            />
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  )
+}
+
+Schedule.propTypes = {Trip: PropTypes.object, setFormElement: PropTypes.func}
+
+export default Schedule
