eb4cfeaba20dcda7b8933460b385e973828f5fe6
Matt McNaney <mcnaneym@appstate.edu>
2020-10-29 08:57:01 -0400

    Removed deity flag copied from organization list.
    Added trip deletion.
    Edit button now link to form.

--- javascript/TripList/Grid.js
+++ javascript/TripList/Grid.js
@@ -2,43 +2,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Grid = ({trips, edit, deleteRow}) => {
+const Grid = ({trips, deleteRow}) => {
   const deleteItem = (key) => {
-    if (prompt('Are you certain you want to delete this trip') === 'DELETE') {
+    if (
+      prompt(
+        'Are you certain you want to delete this trip?\nIf so, type DELETE below.'
+      ) === 'DELETE'
+    ) {
       deleteRow(key)
     }
   }
 
   const deleteButton = (key) => {
-    if (deity === false) {
-      return <span></span>
-    } else {
-      return (
-        <button
-          className="btn btn-sm btn-danger"
-          onClick={() => {
-            deleteItem(key)
-          }}>
-          Delete
-        </button>
-      )
-    }
+    return (
+      <button
+        className="btn btn-sm btn-danger"
+        onClick={() => {
+          deleteItem(key)
+        }}>
+        Delete
+      </button>
+    )
   }
 
   const rows = trips.map((value, key) => {
     return (
       <tr key={'gridrow-' + key}>
         <td style={{width: '15%'}}>
-          <button
+          <a
             className="btn btn-sm btn-primary mr-1"
-            onClick={() => {
-              edit(value.id)
-            }}>
+            href={'triptrack/Admin/Trip/' + value.id + '/edit'}>
             Edit
-          </button>
+          </a>
           {deleteButton(key)}
         </td>
-        <td>{value.name}</td>
+        <td>{value.submitName}</td>
+        <td>{value.destinationCity}</td>
+        <td>{value.memberCount}</td>
       </tr>
     )
   })
@@ -46,7 +46,15 @@ const Grid = ({trips, edit, deleteRow}) => {
   return (
     <div>
       <table className="table table-striped">
-        <tbody>{rows}</tbody>
+        <tbody>
+          <tr>
+            <td></td>
+            <th>Submitter</th>
+            <th>Destination city</th>
+            <th>Participants</th>
+          </tr>
+          {rows}
+        </tbody>
       </table>
     </div>
   )
