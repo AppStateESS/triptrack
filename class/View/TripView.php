dedf2a08d54869062f1d9af421ac221679ea5850
Matt McNaney <mcnaneym@appstate.edu>
2020-10-29 08:53:56 -0400

    Added dashboard to create view
    Added json view.

--- class/View/TripView.php
+++ class/View/TripView.php
@@ -8,6 +8,8 @@
 namespace triptrack\View;
 
 use triptrack\Factory\SettingFactory;
+use phpws2\Database;
+use triptrack\Factory\TripFactory;
 
 class TripView extends AbstractView
 {
@@ -23,7 +25,22 @@ class TripView extends AbstractView
         $settings = SettingFactory::getAll();
         $vars['allowInternational'] = (bool) $settings['allowInternational'];
         $vars['contactBannerRequired'] = (bool) $settings['contactBannerRequired'];
-        return $this->scriptView('Create', $vars);
+        $tpl['dashboard'] = $this->scriptView('Create', $vars);
+        $orgExists = \triptrack\Factory\OrganizationFactory::exists();
+        $tpl['tripActive'] = ' active';
+        $tpl['orgActive'] = null;
+        $tpl['memberActive'] = null;
+        $tpl['settingActive'] = null;
+        $tpl['alert'] = !$orgExists;
+        $template = new \phpws2\Template($tpl);
+        $template->setModuleTemplate('triptrack', 'Admin/Dashboard.html');
+        return $template->get();
+    }
+
+    public function json(int $tripId)
+    {
+        $trip = TripFactory::load(TripFactory::build(), $tripId);
+        return $trip->getVariablesAsValue();
     }
 
 }
