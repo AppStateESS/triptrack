ec9148857f50062a681b449078a211dfaa5c4211
Matt McNaney <mcnaneym@appstate.edu>
2020-10-29 08:52:30 -0400

    New methods post, build, removeAllMembers, and delete

--- class/Factory/TripFactory.php
+++ class/Factory/TripFactory.php
@@ -11,7 +11,7 @@ use phpws2\Database;
 use Canopy\Request;
 use triptrack\Resource\Trip;
 
-class TripFactory
+class TripFactory extends BaseFactory
 {
 
     public static function deleteByOrganizationId(int $organizationId)
@@ -29,12 +29,60 @@ class TripFactory
         $db->delete();
     }
 
+    public static function build()
+    {
+        $trip = new Trip;
+        return $trip;
+    }
+
     public static function list(array $options = [])
     {
         $db = Database::getDB();
         $tbl = $db->addTable('trip_trip');
         $tbl->addOrderBy('destinationCity');
         return $db->select();
+    }
+
+    public static function post(Request $request)
+    {
+        $trip = new Trip;
+        $trip->host = $request->pullPostString('host');
+        $trip->contactName = $request->pullPostString('contactName');
+        $trip->contactEmail = $request->pullPostString('contactEmail');
+        $trip->contactPhone = $request->pullPostString('contactPhone');
+        $trip->destinationCity = $request->pullPostString('destinationCity');
+        $trip->destinationCountry = $request->pullPostString('destinationCountry');
+        $trip->destinationState = $request->pullPostString('destinationState');
+        $trip->housingAddress = $request->pullPostString('housingAddress');
+        $trip->organizationId = $request->pullPostInteger('organizationId');
+        $trip->secContactName = $request->pullPostString('secContactName');
+        $trip->secContactEmail = $request->pullPostString('secContactEmail');
+        $trip->secContactPhone = $request->pullPostString('secContactPhone');
+        $trip->submitEmail = $request->pullPostString('submitEmail');
+        $trip->submitName = $request->pullPostString('submitName');
+        $trip->submitUsername = $request->pullPostString('submitUsername');
+        $trip->timeDeparting = $request->pullPostString('timeDeparting');
+        $trip->timeEventStarts = $request->pullPostString('timeEventStarts');
+        $trip->timeReturn = $request->pullPostString('timeReturn');
+        $trip->visitPurpose = $request->pullPostString('visitPurpose');
+
+        return $trip;
+    }
+
+    public static function removeAllMembers(int $tripId)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('trip_membertotrip');
+        $tbl->addFieldConditional('tripId', $tripId);
+        $db->delete();
+    }
+
+    public static function delete(int $tripId)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('trip_trip');
+        $tbl->addFieldConditional('id', $tripId);
+        $db->delete();
     }
 
 }
