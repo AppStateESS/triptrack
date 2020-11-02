5c5e6b37e00f96ef1a0a9eb0346cac67f36ed984
Matt McNaney <mcnaneym@appstate.edu>
2020-11-02 12:44:47 -0500

    Removed default Props and centered text

--- javascript/Share/Message.js
+++ javascript/Share/Message.js
@@ -1,18 +1,14 @@
 'use strict'
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-const Message = ({message, type}) => {
+const Message = ({message, type = 'danger'}) => {
   if (message === null) {
     return ''
   }
-  return <div className={'alert alert-' + type}>{message}</div>
+  return <div className={'text-center alert alert-' + type}>{message}</div>
 }
 
 Message.propTypes = {message: PropTypes.string, type: PropTypes.string}
 
-Message.defaultProps = {
-  message: '',
-  type: 'danger',
-}
 export default Message
