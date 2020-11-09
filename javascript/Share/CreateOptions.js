6192218fc5c302c384fa2dda35efedcaec3b3409
Matt McNaney <mcnaneym@appstate.edu>
2020-11-09 13:45:57 -0500

    Error check on array length.
    Added object check on value.
    Added idName and labelName params for an array of objects.

--- javascript/Share/CreateOptions.js
+++ javascript/Share/CreateOptions.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-const createOptions = (options) => {
+const createOptions = (options, idName = null, labelName = null) => {
   const inputs = []
-  options.forEach((element) => {
-    inputs.push(<option key={element}>{element}</option>)
-  })
-  return inputs
+  if (options.length > 0) {
+    options.forEach((element) => {
+      if (typeof element === 'string') {
+        inputs.push(<option key={element}>{element}</option>)
+      } else {
+        inputs.push(
+          <option value={element[idName]} key={'key-' + element[idName]}>
+            {element[labelName]}
+          </option>
+        )
+      }
+    })
+    return inputs
+  } else {
+    return null
+  }
 }
 
 export {createOptions}
