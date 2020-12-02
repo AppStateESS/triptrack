d6e0e58fa2c30a2825d21563b0f5935696a5eafb
Matt McNaney <mcnaneym@appstate.edu>
2020-12-02 08:51:01 -0500

    Added ability to send array of labels for concatenation.

--- javascript/Share/CreateOptions.js
+++ javascript/Share/CreateOptions.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
 const createOptions = (options, idName = null, labelName = null) => {
+  const dash = (key) => {
+    return key > 0 ? ' - ' : ''
+  }
   const inputs = []
   if (options.length > 0) {
     options.forEach((element) => {
       if (typeof element === 'string') {
         inputs.push(<option key={element}>{element}</option>)
       } else {
+        let labelValue
+        if (typeof labelName === 'object') {
+          labelValue = labelName.map((value, key) => {
+            return dash(key) + element[value]
+          })
+        } else {
+          labelValue = element[labelName]
+        }
         inputs.push(
           <option value={element[idName]} key={'key-' + element[idName]}>
-            {element[labelName]}
+            {labelValue}
           </option>
         )
       }
