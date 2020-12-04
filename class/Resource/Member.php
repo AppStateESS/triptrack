128c23dc6957cbb884a124c344abd789bf57f2a5
Matt McNaney <mcnaneym@appstate.edu>
2020-12-04 11:52:49 -0500

    Added setPhone to strip non-numeric
    Fixed spaces on constructors from editor bug.

--- class/Resource/Member.php
+++ class/Resource/Member.php
@@ -22,11 +22,17 @@ class Member extends AbstractResource
     {
         $this->bannerId = new \phpws2\Variable\IntegerVar(0, 'bannerId');
         $this->email = new \phpws2\Variable\Email(null, 'email', 100);
-        $this->firstName = new \ phpws2\Variable\TextOnly(null, 'firstName', 50);
-        $this->phone = new \ phpws2\Variable\TextOnly(null, 'phone', 15);
+        $this->firstName = new \phpws2\Variable\TextOnly(null, 'firstName', 50);
+        $this->phone = new \phpws2\Variable\TextOnly(null, 'phone', 15);
         $this->lastName = new \phpws2\Variable\TextOnly(null, 'lastName', 50);
         $this->username = new \phpws2\Variable\TextOnly(null, 'username', 50);
         parent::__construct();
+    }
+
+    public function setPhone($phone)
+    {
+        $strippedPhone = preg_replace('/\D/', '', $phone);
+        $this->phone = $strippedPhone;
     }
 
 }
