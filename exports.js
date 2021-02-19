b054cc19f4cad530ed892a1b587693ceef980154
Matt McNaney <mcnaneym@appstate.edu>
2021-02-19 07:53:19 -0500

    Separating forms

--- exports.js
+++ exports.js
@@ -3,7 +3,8 @@ exports.path = require('path')
 exports.APP_DIR = exports.path.resolve(__dirname, 'javascript')
 
 exports.entry = {
-  Create: exports.APP_DIR + '/Create/index.jsx',
+  AdminTripForm: exports.APP_DIR + '/AdminTripForm/index.jsx',
+  MemberTripForm: exports.APP_DIR + '/MemberTripForm/index.jsx',
   OrgList: exports.APP_DIR + '/OrgList/index.jsx',
   TripList: exports.APP_DIR + '/TripList/index.jsx',
   ImportForm: exports.APP_DIR + '/ImportForm/index.jsx',
