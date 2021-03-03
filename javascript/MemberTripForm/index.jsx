c00ec8eacb40eea76a8a5637f03ebfff69b785d0
Matt McNaney <mcnaneym@appstate.edu>
2021-03-03 12:41:30 -0500

    Fleshed out memberForm placeholder.

--- javascript/MemberTripForm/index.jsx
+++ javascript/MemberTripForm/index.jsx
@@ -1,11 +1,24 @@
 'use strict'
 import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import Form from '../TripForm/Form'
 
-const MemberTripForm = ({}) => {
-  return <div>MemberTripForm</div>
+const MemberTripForm = (props) => {
+  return (
+    <div>
+      <Form {...props} allowApproval={false} role="Member" />
+    </div>
+  )
 }
 
-MemberTripForm.propTypes = {}
-
-export default MemberTripForm
+ReactDOM.render(
+  <MemberTripForm
+    allowInternational={allowInternational}
+    contactBannerRequired={contactBannerRequired}
+    tripId={tripId}
+    defaultState={defaultState}
+    defaultCountry={defaultCountry}
+  />,
+  document.getElementById('MemberTripForm')
+)
