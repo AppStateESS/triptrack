c47e3ec78d5b9fe26e43c877220a4fbb8435f37c
Matt McNaney <mcnaneym@appstate.edu>
2021-03-03 13:49:20 -0500

    Submit name and email are current user if member. Input not available.

--- javascript/TripForm/Submitter.js
+++ javascript/TripForm/Submitter.js
@@ -3,27 +3,63 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Organizations from './Organizations'
 
-const Submitter = ({Trip, setFormElement, errorCheck, backup, errors}) => {
+const Submitter = ({
+  Trip,
+  setFormElement,
+  errorCheck,
+  backup,
+  errors,
+  role,
+}) => {
   const invalid = 'form-control is-invalid'
   const valid = 'form-control'
+
+  const submitName = () => {
+    if (role === 'Member') {
+      return <strong>{Trip.submitName}</strong>
+    } else {
+      return (
+        <input
+          type="text"
+          name="submitName"
+          className={errors.submitName ? invalid : valid}
+          value={Trip.submitName}
+          onBlur={() => errorCheck('submitName')}
+          onChange={(e) => {
+            setFormElement('submitName', e.target.value)
+          }}
+        />
+      )
+    }
+  }
+
+  const submitEmail = () => {
+    if (role === 'Member') {
+      return <strong>{Trip.submitEmail}</strong>
+    } else {
+      return (
+        <input
+          type="text"
+          name="submitEmail"
+          className={errors.submitEmail ? invalid : valid}
+          value={Trip.submitEmail}
+          onBlur={() => errorCheck('submitEmail')}
+          onChange={(e) => {
+            setFormElement('submitEmail', e.target.value)
+          }}
+        />
+      )
+    }
+  }
 
   return (
     <fieldset>
       <legend className="border-bottom mb-3">Submitter information</legend>
-      <Organizations Trip={Trip} setFormElement={setFormElement} />
+      <Organizations Trip={Trip} setFormElement={setFormElement} role={role} />
       <div className="row form-group">
         <div className="col-sm-4">Your name</div>
         <div className="col-sm-8">
-          <input
-            type="text"
-            name="submitName"
-            className={errors.submitName ? invalid : valid}
-            value={Trip.submitName}
-            onBlur={() => errorCheck('submitName')}
-            onChange={(e) => {
-              setFormElement('submitName', e.target.value)
-            }}
-          />
+          {submitName()}
           {errors.submitName ? (
             <div className="invalid-feedback">Please provide a valid name.</div>
           ) : null}
@@ -32,16 +68,8 @@ const Submitter = ({Trip, setFormElement, errorCheck, backup, errors}) => {
       <div className="row form-group">
         <div className="col-sm-4">Your email</div>
         <div className="col-sm-8">
-          <input
-            type="text"
-            name="submitEmail"
-            className={errors.submitEmail ? invalid : valid}
-            value={Trip.submitEmail}
-            onBlur={() => errorCheck('submitEmail')}
-            onChange={(e) => {
-              setFormElement('submitEmail', e.target.value)
-            }}
-          />
+          {submitEmail()}
+
           {errors.submitEmail ? (
             <div className="invalid-feedback">
               Please provide a valid email address.
