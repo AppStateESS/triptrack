306e9633ffddc5e8134233870a8826d451105a92
Matt McNaney <mcnaneym@appstate.edu>
2021-05-03 07:41:37 -0400

    Date change.

--- config/defines.dist.php
+++ config/defines.dist.php
@@ -2,7 +2,7 @@
 
 /**
  * MIT License
- * Copyright (c) 2019 Electronic Student Services @ Appalachian State University
+ * Copyright (c) 2021 Electronic Student Services @ Appalachian State University
  *
  * See LICENSE file in root directory for copyright and distribution permissions.
  *
