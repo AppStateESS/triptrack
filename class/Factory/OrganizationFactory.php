754598e5fedfc8472f3d325377be166c2a6d4f5e
Matt McNaney <mcnaneym@appstate.edu>
2021-05-25 07:45:55 -0400

    Added option to organization to only return rows associated...
    with member id.

--- class/Factory/OrganizationFactory.php
+++ class/Factory/OrganizationFactory.php
@@ -34,16 +34,30 @@ class OrganizationFactory extends BaseFactory
         $db = Database::getDB();
         $tbl = $db->addTable('trip_organization');
         if (!empty($options['memberCount'])) {
-            $tbl2 = $db->addTable('trip_membertoorg');
+            $tbl2 = $db->addTable('trip_membertoorg', null, false);
             $counter = $tbl2->addField('memberId', 'memberCount');
             $counter->showCount();
-            $joinConditional = $db->createConditional($tbl->getField('id'),
-                    $tbl2->getField('organizationId'));
-            $db->joinResources($tbl, $tbl2, $joinConditional, 'left');
-            $db->setGroupBy([$tbl->getField('id')]);
+            self::joinOrgWithMember($db, $tbl, $tbl2);
+        }
+
+        if (!empty($options['memberId'])) {
+            if (!isset($tbl2)) {
+                $tbl2 = $db->addTable('trip_membertoorg', null, false);
+                self::joinOrgWithMember($db, $tbl, $tbl2);
+            }
+            $tbl2->addFieldConditional('memberId', $options['memberId']);
         }
         $tbl->addOrderBy('name');
         return $db->select();
+    }
+
+    private static function joinOrgWithMember(\phpws2\Database\DB $db,
+            \phpws2\Database\Table $orgTable, \phpws2\Database\Table $pivotTable)
+    {
+        $joinConditional = $db->createConditional($orgTable->getField('id'),
+                $pivotTable->getField('organizationId'));
+        $db->joinResources($orgTable, $pivotTable, $joinConditional, 'left');
+        $db->setGroupBy([$orgTable->getField('id')]);
     }
 
     public static function post(Request $request)
