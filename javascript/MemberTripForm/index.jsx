8baad112861e8bb0c7f2b56083c2fa2f90f01847
Matt McNaney <mcnaneym@appstate.edu>
2021-06-07 10:19:49 -0400

    Moved functions from Form into the parent.
    Using multi promise to prevent extra renders.
    Using view switch for loading, error, and content.

--- javascript/MemberTripForm/index.jsx
+++ javascript/MemberTripForm/index.jsx
@@ -1,22 +1,77 @@
 'use strict'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import Form from '../TripForm/Form'
+import {getTrip} from '../api/TripAjax'
+import {getList} from '../api/Fetch'
+import Loading from '../api/Loading'
+import NoMemberOrg from './NoMemberOrg'
 
 /* global allowInternational, contactBannerRequired, tripId, defaultState, defaultCountry, hostLabel, organizationLabel, accommodationRequired, secondaryRequired */
 
 const MemberTripForm = (props) => {
-  return (
-    <div>
-      <Form
-        {...props}
-        allowApproval={false}
-        role="Member"
-        hostLabel={hostLabel}
-        organizationLabel={organizationLabel}
-      />
-    </div>
-  )
+  const [organizations, setOrganizations] = useState([])
+  const [view, setView] = useState('loading')
+  const [defaultTrip, setDefaultTrip] = useState({})
+
+  const loadOrganizations = () => {
+    return getList('./triptrack/Member/Organization/')
+  }
+
+  useEffect(() => {
+    Promise.all([loadOrganizations(), getTrip(0, 'Member')])
+      .then((response) => {
+        const orgList = response[0].data
+        const trip = response[1].data
+        if (trip.contactPhone !== '') {
+          trip.contactPhone = trip.contactPhone.toString()
+        }
+        if (trip.secContactPhone !== '') {
+          trip.secContactPhone = trip.secContactPhone.toString()
+        }
+        if (orgList.length === 0) {
+          setView('noorg')
+        } else {
+          if (trip.organizationId == 0) {
+            trip.organizationId = orgList[0].id
+          }
+          setOrganizations(orgList)
+          setDefaultTrip(trip)
+          setView('form')
+        }
+      })
+      .catch((error) => {
+        setView('error')
+      })
+  }, [])
+
+  switch (view) {
+    case 'loading':
+      return <Loading />
+
+    case 'error':
+      return (
+        <div className="alert alert-danger">
+          Error: could not contact the server.
+        </div>
+      )
+
+    case 'form':
+      return (
+        <Form
+          {...props}
+          allowApproval={false}
+          organizations={organizations}
+          defaultTrip={defaultTrip}
+          role="Member"
+          hostLabel={hostLabel}
+          organizationLabel={organizationLabel}
+        />
+      )
+
+    case 'noorg':
+      return <NoMemberOrg organizationLabel={organizationLabel} />
+  }
 }
 
 ReactDOM.render(
@@ -27,9 +82,9 @@ ReactDOM.render(
     accommodationRequired={accommodationRequired}
     secondaryRequired={secondaryRequired}
     hostLabel={hostLabel}
-    tripId={tripId}
     defaultState={defaultState}
     defaultCountry={defaultCountry}
+    tripId={tripId}
   />,
   document.getElementById('MemberTripForm')
 )
