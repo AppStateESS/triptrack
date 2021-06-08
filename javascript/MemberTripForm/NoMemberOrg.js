68641e11d4840ce8bb47bda25bf426fca5ccb4e3
Matt McNaney <mcnaneym@appstate.edu>
2021-06-08 12:48:56 -0400

    Removed unused variables.

--- javascript/MemberTripForm/NoMemberOrg.js
+++ javascript/MemberTripForm/NoMemberOrg.js
@@ -1,5 +1,5 @@
 'use strict'
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 const NoMemberOrg = ({organizationLabel}) => {
--- javascript/MemberTripForm/index.jsx
+++ javascript/MemberTripForm/index.jsx
@@ -40,7 +40,7 @@ const MemberTripForm = (props) => {
           setView('form')
         }
       })
-      .catch((error) => {
+      .catch(() => {
         setView('error')
       })
   }, [])
