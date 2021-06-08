d3358a179631e06d397d85e7c2f2bc8506ecfe80
Matt McNaney <mcnaneym@appstate.edu>
2021-06-08 12:48:02 -0400

    Filled out NoAdminOrg.

--- javascript/AdminTripForm/NoAdminOrg.js
+++ javascript/AdminTripForm/NoAdminOrg.js
@@ -1,11 +1,19 @@
 'use strict'
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-const NoAdminOrg = ({}) => {
-  return <div>NoAdminOrg</div>
+const NoAdminOrg = ({organizationLabel}) => {
+  return (
+    <div>
+      <h3>Sorry</h3>
+      <p>
+        Before you can create a trip, you must create at least one
+        {organizationLabel}.
+      </p>
+    </div>
+  )
 }
 
-NoAdminOrg.propTypes = {}
+NoAdminOrg.propTypes = {organizationLabel: PropTypes.string}
 
 export default NoAdminOrg
