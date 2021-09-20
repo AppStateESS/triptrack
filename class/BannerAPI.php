92732a5988990c13f17edf3f8ba3ef640183199a
Matt McNaney <mcnaneym@appstate.edu>
2021-09-20 12:26:53 -0400

    Updated Banner curl call.

--- class/BannerAPI.php
+++ class/BannerAPI.php
@@ -19,13 +19,40 @@ class BannerAPI
         $pluggedUrl = str_replace('{id}', $identity, $url);
 
         $curl = curl_init();
-        curl_setopt_array($curl, array(CURLOPT_RETURNTRANSFER => 1, CURLOPT_URL => $pluggedUrl));
-        $result = json_decode(curl_exec($curl));
+        curl_setopt_array($curl, array(CURLOPT_RETURNTRANSFER => 1, CURLOPT_URL => $pluggedUrl, CURLOPT_FAILONERROR => 1, CURLOPT_TIMEOUT => 8, CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0));
 
-        if (empty($result->userName)) {
-            return false;
+        $result = curl_exec($curl);
+
+        if (!$result) {
+            $errNo = curl_errno($curl);
+
+            $errorMessage = curl_error($curl);
+            $endProcess = true;
+            switch ($errNo) {
+                case 7:
+                    $error = 'Could not connect to API server.';
+                    break;
+
+                case 28:
+                    $error = 'Request timed out.';
+                    break;
+
+                case 22:
+                    $error = 'Student not found.';
+                    $endProcess = false;
+                    break;
+
+                case '51':
+                    $error = 'Something is wrong with the SSL connection.';
+                    break;
+
+                default:
+                    $error = 'Unknown error: ' . $errorMessage;
+                    break;
+            }
+            return ['success' => false, 'error' => $error, 'errNo' => $errNo, 'endProcess' => $endProcess];
         } else {
-            return $result;
+            return json_decode($result);
         }
     }
 
