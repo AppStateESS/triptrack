d018f39541711962e361d3ef083467f80d976ea8
Matt McNaney <mcnaneym@appstate.edu>
2021-09-23 12:01:39 -0400

    Added confirmation form for trips.
    Resolves #20.

---
+++ javascript/TripForm/Confirmation.js
@@ -0,0 +1,54 @@
+'use strict'
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import BigCheckBox from '@essappstate/canopy-react-bigcheckbox'
+
+const Confirmation = ({
+  confirmationInstructions,
+  completeConfirmation,
+  cancel,
+}) => {
+  const [confirmCheck, setConfirmCheck] = useState(false)
+  return (
+    <div>
+      <p>
+        Please read the travel conditions below. Click on the check box to
+        confirm your adherence to the requirements.
+      </p>
+      <div className="border p-3 mb-3">{confirmationInstructions}</div>
+      <div className="mb-3">
+        <BigCheckBox
+          label={<span>I agree with the above conditions.</span>}
+          checked={confirmCheck}
+          handle={() => setConfirmCheck(true)}
+        />
+      </div>
+      <div>
+        <button
+          className="btn btn-primary btn-block mb-3"
+          disabled={!confirmCheck}
+          onClick={() => {
+            completeConfirmation(confirmCheck)
+          }}>
+          Save trip
+        </button>
+        <button
+          className="btn btn-danger btn-block"
+          onClick={() => {
+            setConfirmCheck(false)
+            cancel()
+          }}>
+          Cancel
+        </button>
+      </div>
+    </div>
+  )
+}
+
+Confirmation.propTypes = {
+  confirmationInstructions: PropTypes.string,
+  completeConfirmation: PropTypes.func,
+  cancel: PropTypes.func,
+}
+
+export default Confirmation
