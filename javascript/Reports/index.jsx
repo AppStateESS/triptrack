4f1f9a2b71c98c478906ccaf46cb2a9f43afe034
Matt McNaney <mcnaneym@appstate.edu>
2021-09-28 12:54:50 -0400

    Added member and trip listing by state.
    Changed report layout.

--- javascript/Reports/index.jsx
+++ javascript/Reports/index.jsx
@@ -11,6 +11,10 @@ const Reports = () => {
   const [orgId, setOrgId] = useState(0)
   const [tripList, setTripList] = useState([])
   const [tripId, setTripId] = useState(0)
+  const [usaState, setUsaState] = useState('')
+  const [currentOrg, setCurrentOrg] = useState()
+  const [upcomingOnly, setUpcomingOnly] = useState(true)
+  const [usStates, setUsStates] = useState([])
 
   useEffect(() => {
     getList('./triptrack/Admin/Organization/').then((response) => {
@@ -18,8 +22,14 @@ const Reports = () => {
     })
   }, [])
 
+  useEffect(() => {
+    getList('./triptrack/Admin/Trip/usedStates').then((response) => {
+      setUsStates(response.data)
+    })
+  }, [])
+
   const loadTrips = () => {
-    getList('./triptrack/Admin/Trip/').then((response) => {
+    getList('./triptrack/Admin/Trip/', {upcomingOnly}).then((response) => {
       setTripList(response.data)
     })
   }
@@ -30,78 +40,175 @@ const Reports = () => {
     } else {
       loadTrips()
     }
+  }, [orgId, upcomingOnly])
+
+  useEffect(() => {
+    const found = orgList.find((value) => {
+      return value.id === parseInt(orgId)
+    })
+    if (found) setCurrentOrg(found.name)
   }, [orgId])
 
-  const orgOptions = (
-    <div className="col-sm-6">
-      <div className="mb-2">
-        <select
-          name="organization"
-          className="form-control"
-          value={orgId}
-          onChange={(e) => setOrgId(e.target.value)}>
-          <option>Choose a {organizationLabel} below:</option>
-          {orgList.map((value) => {
-            return (
-              <option key={`org-${value.id}`} value={value.id}>
-                {value.name}
-              </option>
-            )
-          })}
-        </select>
-      </div>
+  const aOrAn = () => {
+    return organizationLabel.match(/^[aeiou]/i) ? 'an' : 'a'
+  }
+  const stateOptions = (
+    <select
+      name="states"
+      className="form-control mb-2"
+      value={usaState}
+      onChange={(e) => setUsaState(e.target.value)}>
+      <option>Choose a state below:</option>
+      {usStates.map((value, key) => {
+        return (
+          <option key={`state-${key}`} value={value}>
+            {value}
+          </option>
+        )
+      })}
+    </select>
+  )
 
-      {orgId > 0 ? (
-        <div className="text-center">
-          <a
-            href={`./triptrack/Admin/Report/organization/?orgId=${orgId}`}
-            className="btn btn-primary">
-            Get {organizationLabel} member list
-          </a>
-        </div>
-      ) : null}
-    </div>
+  const orgOptions = (
+    <select
+      name="organization"
+      className="form-control"
+      value={orgId}
+      onChange={(e) => setOrgId(e.target.value)}>
+      <option>
+        Choose {aOrAn()} {organizationLabel} below:
+      </option>
+      {orgList.map((value) => {
+        return (
+          <option key={`org-${value.id}`} value={value.id}>
+            {value.name}
+          </option>
+        )
+      })}
+    </select>
   )
 
   let tripOptions
 
-  if (orgId > 0) {
-    tripOptions = (
-      <div className="col-sm-6">
-        <div className="mb-2">
-          <select
-            name="trip"
-            className="form-control"
-            value={tripId}
-            onChange={(e) => setTripId(e.target.value)}>
-            <option>Choose a trip below:</option>
-            {tripList.map((value) => {
-              return (
-                <option key={`trip-${value.id}`} value={value.id}>
-                  {value.host}{' '}
-                  {dayjs(value.timeEventStarts * 1000).format('MMM D, YYYY')} (
-                  {value.approved == 1 ? 'Approved' : 'Not approved'})
-                </option>
-              )
-            })}
-          </select>
-        </div>
-        {tripId > 0 ? (
-          <div className="text-center">
-            <a
-              href={`./triptrack/Admin/Report/trip/?tripId=${tripId}`}
-              className="btn btn-primary">
-              Get trip member list
-            </a>
-          </div>
-        ) : null}
-      </div>
+  let firstOption = <option>Choose a trip below:</option>
+  if (orgId == 0) {
+    firstOption = (
+      <option>
+        Choose {aOrAn()} {organizationLabel} above
+      </option>
+    )
+  } else if (tripList.length === 0) {
+    firstOption = (
+      <option>
+        No {upcomingOnly ? 'upcoming' : null} trips found for {currentOrg}
+      </option>
     )
   }
+
+  const disabled = orgId == 0 || tripList.length == 0
+  tripOptions = (
+    <div>
+      <select
+        name="trip"
+        disabled={disabled}
+        className="form-control"
+        value={tripId}
+        onChange={(e) => setTripId(e.target.value)}>
+        {firstOption}
+        {tripList.map((value) => {
+          return (
+            <option key={`trip-${value.id}`} value={value.id}>
+              {value.host}{' '}
+              {dayjs(value.timeEventStarts * 1000).format('MMM D, YYYY')} (
+              {value.approved == 1 ? 'Approved' : 'Not approved'})
+            </option>
+          )
+        })}
+      </select>
+    </div>
+  )
   return (
-    <div className="row">
-      {orgOptions}
-      {tripOptions}
+    <div>
+      <div className="mb-3 text-center">
+        <button
+          className={`mr-2 btn ${upcomingOnly ? 'btn-success' : 'btn-danger'}`}
+          onClick={() => {
+            setUpcomingOnly(true)
+          }}>
+          Upcoming trips only
+        </button>
+        <button
+          className={`btn ${upcomingOnly ? 'btn-danger' : 'btn-success'}`}
+          onClick={() => {
+            setUpcomingOnly(false)
+          }}>
+          Show all trips
+        </button>
+      </div>
+      <table className="table">
+        <tbody>
+          <tr>
+            <td>
+              <div className="mb-2">{orgOptions}</div>
+            </td>
+
+            <td>
+              <div>
+                <button
+                  disabled={disabled}
+                  onClick={() => {
+                    location.href = `./triptrack/Admin/Report/organization/?orgId=${orgId}`
+                  }}
+                  className="btn btn-primary">
+                  <i className="fas fa-download"></i> {organizationLabel} member
+                  list
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div>{tripOptions}</div>
+            </td>
+
+            <td>
+              <div className="mb-2">
+                <button
+                  onClick={() =>
+                    (location.href = `./triptrack/Admin/Report/trip/?tripId=${tripId}&upcomingOnly`)
+                  }
+                  disabled={disabled || tripId === 0}
+                  className="btn btn-primary">
+                  <i className="fas fa-download"></i> Trip member list
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>{stateOptions}</td>
+            <td>
+              <button
+                className="btn btn-primary mr-2"
+                disabled={usaState.length == 0}
+                onClick={() =>
+                  (location.href = `./triptrack/Admin/Report/stateMembers/?state=${usaState}&upcomingOnly=${upcomingOnly}`)
+                }>
+                <i className="fas fa-download"></i>{' '}
+                {upcomingOnly ? 'Upcoming' : 'All'} state trip members
+              </button>
+              <button
+                className="btn btn-primary"
+                disabled={usaState.length == 0}
+                onClick={() =>
+                  (location.href = `./triptrack/Admin/Report/stateTrips/?state=${usaState}&upcomingOnly=${upcomingOnly}`)
+                }>
+                <i className="fas fa-download"></i>{' '}
+                {upcomingOnly ? 'Upcoming' : 'All'} state trips
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
