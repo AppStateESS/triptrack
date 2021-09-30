656d9e453e3e889fcfdf0ef47a17e30fbf580b68
Matt McNaney <mcnaneym@appstate.edu>
2021-09-30 14:51:08 -0400

    Added engageId column to link to trip_engageOrg

--- class/Resource/Organization.php
+++ class/Resource/Organization.php
@@ -11,11 +11,13 @@ class Organization extends AbstractResource
 {
 
     protected $name;
+    protected $engageId = 0;
     protected $table = 'trip_organization';
 
     public function __construct()
     {
         $this->name = new \phpws2\Variable\TextOnly('', 'name', 255);
+        $this->engageId = new \phpws2\Variable\IntegerVar(0, 'engageId');
         parent::__construct();
     }
 
