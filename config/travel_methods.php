89c33c9aca476190d386b448ebf5e4a644ea88a6
Matt McNaney <mcnaneym@appstate.edu>
2022-02-02 08:55:02 -0500

    Added travel methods.

---
+++ config/travel_methods.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+/**
+ * A listing of travel options. The array must remain sequential to work with TravelMethods.js in the javascript
+ * directory. Changes here need to be reflected there as well.
+ *
+ * Personal is the default
+ */
+define('TT_PERSONAL', 0);
+define('TT_CAR_SHARE', 1);
+define('TT_UNI_VAN', 2);
+define('TT_RENTAL', 3);
+define('TT_BUS', 4);
+define('TT_AIR', 5);
+define('TT_TRAIN', 6);
+
+/**
+ *  future proofing
+ */
+define('TT_BEAMED_TO_PLANET', 38);
+define('TT_IMPROBABLE_HEART_OF_GOLD_PICKUP', 42);
+define('TT_FLUX_C_DELOREAN', 71);
+define('TT_RONCO_JETPAQ4000', 77);
+define('TT_CLONE_TRANSFERAL', 88);
+//define('TT_NO_I_AM_THE_ORIGINAL_DONT_INCINERATE_ME_PLZ', 88);
+define('TT_AMAZONASA_MOON_SHUTTLE', 91);
+define('TT_ARMED_TRANSPORT_THRU_VACCINE_MUTTIE_TOWN', 94);
+define('TT_X_Æ_A_12_IMPERIAL_BATTLECRUISER', 95);
+define('TT_WHIPPED_FORWARD_BY_TALKING_APE', 101);
+define('TT_IDK_SOME_KIND_OF_STAR_WARS_TOY_THE_DEWBACK_MAYBE_THE_STORMTROOPER_COULD_BE_PUSHED_STRAIGHT_DOWN_IN_IT_THAT_WAS_PRETTY_COOL_RIGHT_CAUSE_IT_LOOKED_LIKE_HE_WAS_RIDING_IT_THE_M_FALCON_WOULD_BE_BETTER_BUT_WHO_COULD_AFFORD_THAT', 121);
