1990a5015ccb3b67cd9054b305f38d24c6b47b3e
Matt McNaney <mcnaneym@appstate.edu>
2022-02-02 14:12:49 -0500

    Fixed the version_compare
    Added travelMethod field to trip_trip table.

--- boost/update.php
+++ boost/update.php
@@ -12,14 +12,25 @@
 function triptrack_update(&$content, $currentVersion)
 {
     switch ($currentVersion) {
-        case version_compare($currentVersion, '1.1.0'):
+        case version_compare($currentVersion, '1.1.0', '<'):
             $content[] = 'Added organizationId to trip list properties.';
             $content[] = 'Added link to member listing by organization in admin view.';
             $content[] = 'Created front page interaction';
             $content[] = 'Added check all button for member selection';
-        case version_compare($currentVersion, '1.2.0'):
+        case version_compare($currentVersion, '1.2.0', '<'):
+            tt_update_1_2_0($content);
             $content[] = 'Added setting for Engage organizations only.';
             $content[] = 'Organization form changed to only allow organizations in Engage is setting is set.';
     }
     return true;
+}
+
+function tt_update_1_2_0(&$content)
+{
+    $db = \phpws2\Database::getDB();
+    $tbl = $db->addTable('trip_trip');
+    $travelMethod = $tbl->addDataType('travelMethod', 'smallint');
+    $travelMethod->setDefault(0);
+    $travelMethod->add();
+    $content[] = 'Added travelMethod column to trip_trip table.';
 }
