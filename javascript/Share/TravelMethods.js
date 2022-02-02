42f417a57956150ee94732325f895bdaed510b6b
Matt McNaney <mcnaneym@appstate.edu>
2022-02-02 09:04:03 -0500

    JS file of travel methods for import

---
+++ javascript/Share/TravelMethods.js
@@ -0,0 +1,17 @@
+/**
+ * This array cooresponds directly with the travel_methods.php file in the config directory.
+ * Changes there should be reflected here as well. Breaks in sequential numbering will cause
+ * problems.
+ */
+
+const travelMethods = [
+  'Personal',
+  'Car share',
+  'University Van',
+  'Rental vehicle',
+  'Bus',
+  'Flight',
+  'Train',
+]
+
+export {travelMethods}
